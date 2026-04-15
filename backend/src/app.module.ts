6e0bbe77b3f8e8108be120e77622983ea856fb0c
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2026-04-15 19:13:58 +0300

    created genre entities and reviews

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilmsModule } from './films/films.module';
+import { GenresModule } from './genres/genres.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [FilmsModule],
+  imports: [FilmsModule, GenresModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
